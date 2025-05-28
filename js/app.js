fd9b843d8bfe8043aafef5156487d7dceee46c4a
Vladimir Jeanfranco Paz Fajardo - HP <vladimirpaz@bcp.com.pe>
2025-05-28 15:24:50 -0500

    Tercer Avance

--- js/app.js
+++ js/app.js
@@ -1,2 +1,11 @@
 let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a'); 
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () =>{
+    sections.forEach(sec =>{
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+    })
+}
